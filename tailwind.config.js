0dc2b51c1fc61f3bbd0f4f5353d69e414dce347f
ismailhossenpub <ismailhossenpub@gmail.com>
2021-06-20 23:42:59 +0600

    E-commerce site

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lb:'992px',
+      xl:'1200px'
+    },
+    container:{
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily:{
+        poppins:"'Poppins', sans-serif",
+        roboto:"'Roboto', sans-serif"
+      },
+      colors:{
+        'primary':"#FD3D57"
+      }
+    },
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+      visibility: ['group-hover']
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
